accaf8336c85268ac485ef770b7fc9aec2672771
Gabriel Nascimento <gabrielgrames@gmailcom>
2020-01-14 14:59:03 -0400

    api ready

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+
+mongoose
+  .connect("mongodb://localhost/omnistack", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Conectado ao banco de dados "))
+  .catch(err => console.log(err));
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333);
