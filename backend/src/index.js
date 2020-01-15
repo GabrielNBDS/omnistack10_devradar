1ca4f14ca8de8a14e4a80b632119a949144b01dc
Gabriel Nascimento <gabrielgrames@gmailcom>
2020-01-15 13:38:16 -0400

    Web Interface Ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
@@ -12,8 +13,8 @@ mongoose
   .then(() => console.log("Conectado ao banco de dados "))
   .catch(err => console.log(err));
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(3333);
